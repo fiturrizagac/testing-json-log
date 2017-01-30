767fe043d565e0fd4381c8d7ccaa5ec9c082ee64
Franco Iturrizaga <fiturrizagac@gmail.com>
2017-01-30 08:33:26 -0500

    Adding MDC

--- src/main/java/com/fiturriz/logging/App.java
+++ src/main/java/com/fiturriz/logging/App.java
@@ -1,7 +1,14 @@
 package com.fiturriz.logging;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -22,13 +29,39 @@ public class App {
     }
 
     @RequestMapping("/test")
-    public ResponseEntity method() {
+    public ResponseEntity method() throws Exception {
         logger.error("holy shit!");
         logger.warn("holy shit!");
         logger.info("holy shit!");
         logger.debug("holy shit!");
         logger.trace("holy shit!");
+
+
+        loggerMDC();
+
         return ResponseEntity.ok().build();
+    }
+
+
+
+    protected void loggerMDC() throws Exception{
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String,Object> json  = new HashMap<>();
+        json.put("key1","valor1");
+        json.put("key2","valor2");
+        json.put("key3","valor3");
+
+        MDC.put("unJson",mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+
+        Gson gson = new GsonBuilder().create();
+
+        MDC.put("unJson2",gson.toJson(json));
+
+        logger.info("mdc PS");
+        MDC.clear();
     }
 
 }
