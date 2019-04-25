94cce4fc45b3c39e8d9485fb43387f10e2a39430
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-04-25 08:36:47 -0500

    Create script folder

--- scripts/release-candidate.sh
+++ scripts/release-candidate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn release:prepare -DtagNameFormat="RC-v@{project.version}" -B
-current_version=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+current_version=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.scm.tag}' --non-recursive exec:exec)
 mvn release:perform -B
 mvn release:clean release:branch \
     -DbranchBase=master \
