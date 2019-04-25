3cc61f382080b83408844b53124aa889bedae2f1
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-04-25 09:18:04 -0500

    Add checkout configuration

--- scripts/release-candidate.sh
+++ scripts/release-candidate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git checkout develop
+
 tag_prefix="RC-v"
 
 mvn release:prepare -DtagNameFormat="$tag_prefix@{project.version}" -B
@@ -7,6 +9,8 @@ mvn release:prepare -DtagNameFormat="$tag_prefix@{project.version}" -B
 tag_name=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.scm.tag}' --non-recursive exec:exec)
 
 mvn release:perform -B
+
+git checkout .
 
 git checkout $tag_name
 
