89e076430bed7679d5bd390da6c19d4c345c1aff
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-04-25 07:00:39 -0500

    change script

--- release-candidate.sh
+++ release-candidate.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 mvn release:prepare -B
+current_version=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 mvn release:perform -B
+mvn release:clean release:branch \
+    -DbranchBase=master \
+    -DbranchName=release \
+    -DsuppressCommitBeforeBranch=true -DremoteTagging=false \
+    -DupdateWorkingCopyVersions=false -B
+
